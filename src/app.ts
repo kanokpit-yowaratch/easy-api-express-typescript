4774795e877ea917eb6bc8958e1e869980d8bb29
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2023-12-23 20:26:24 +0700

    Add nodemon to run dev in watch mode

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/salt', (req, res) => {
+    res.send('เค็ม แต่ดี');
+});
+
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
